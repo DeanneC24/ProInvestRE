7ada6d67baf754e1c1260c35e4c98de2137f5531
Deanne Clarke <deanneclarke@hotmail.co.uk>
2023-09-14 21:11:28 +0100

    Added basic express server framework

--- build/index.js
+++ build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://localhost:${PORT}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
-export function add(x: number, y: number): number {
-    return x + y;
-}
+import cors from "cors"
+import express, { Application, NextFunction, Request, Response, query } from "express"
+ 
+const PORT = 8080;
+ 
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.listen(PORT, () => {
+    console.log(`Running on http://localhost:${PORT}`);
+});
 
-export function multiply(x: number, y: number): number {
-    return x * y;
-}
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+ 
