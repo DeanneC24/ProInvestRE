fcdf9fe1bbb94a8ebf1e99e7bc3101222b9e06da
Deanne Clarke <deanneclarke@hotmail.co.uk>
2023-09-25 21:25:34 +0100

    Fix for multiple port issue in render

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,8 @@ import cors from "cors"
 import express, { Application, NextFunction, Request, Response, query } from "express"
 import elasticsearchService from './elasticsearch-service/index'
 import authenticationService from './authentication-service/index'
-const PORT = 8080;
-const app = express();
+const PORT = 8080
+const app = express()
 app.use(cors())
 app.use(express.json())
 
@@ -15,10 +15,9 @@ app.use('/auth', authenticationService)
 app.use('/es', elasticsearchService)
 
 app.listen(PORT, () => {
-    console.log(`Main application is running on http://localhost:${PORT}`);
+  console.log(`Main application is running on http://localhost:${PORT}`);
 });
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World From ProInvestRe');
-});
- 
+  res.send('Hello World From ProInvestRe')
+})
